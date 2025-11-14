ec9243f8687ddea2076ba04f375a56917f4e588b
araxxdjoficial-lgtm <araxxdj.oficial@gmail.com>
2025-11-13 20:52:43 -0500

    SQlite change DB

--- myapp/forms.py
+++ myapp/forms.py
@@ -1,8 +1,6 @@
 from django import forms
 from .models import Project
 
-projects = list(Project.objects.values())
-
 class CreateNewTask(forms.Form):
     title = forms.CharField(label="Titulo de tarea", max_length=200, widget=forms.TextInput(attrs={'class': 'input'}))
     description=forms.CharField(label="descripcion de la tarea", widget=forms.Textarea(attrs={'class': 'input'}))
--- taskSite/settings.py
+++ taskSite/settings.py
@@ -91,7 +91,8 @@ WSGI_APPLICATION = 'taskSite.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default='postgresql://postgres:postgres@localhost/postgres',
-        conn_max_age=600
+        conn_max_age=600,
+        ssl_require=True
     )
 }
 
